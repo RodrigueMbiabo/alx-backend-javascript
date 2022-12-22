9162aa4bbf4dc6a96fa5cdd316b40a1c3d9c7b1a
rodriguembiabo <rodriguembiabo@gmail.com>
2022-12-22 14:22:06 +0100

    update task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    this.evacuationWarningMessage();
-  }
-
-  evacuationWarningMessage() {
-    if (this.constructor.name !== 'Building') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
   }
 
   get sqft() {
-    return this.sqft;
+    return this._sqft;
   }
 
-  set sqft(sqft) {
-    this._sqft = sqft;
+  set sqft(value) {
+    this._sqft = value;
   }
-  
 }
