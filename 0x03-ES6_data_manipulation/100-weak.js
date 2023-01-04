feeb04441e9dd782e224bce410f953a0f7c48809
rodriguembiabo <rodriguembiabo@gmail.com>
2023-01-04 04:09:12 +0100

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,9 @@
 export const weakMap = new WeakMap();
 
-export const queryAPI = (endpoint) => {
+export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    const endpointData = weakMap.get(endpoint);
-    if (endpointData >= 4) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, (endpointData + 1));
-  } else {
-    weakMap.set(endpoint, 1);
-  }
-};
+    const nQuery = weakMap.get(endpoint);
+    if (nQuery >= 4) throw Error('Endpoint load is high');
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else { weakMap.set(endpoint, 1); }
+}
